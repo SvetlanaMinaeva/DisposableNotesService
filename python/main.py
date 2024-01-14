22cbd907a0513b5c0c189a1f7e37aed67cb15e50
Svetlana Minaeva <svetilnik00001@gmail.com>
2024-01-14 19:07:38 +0300

    templates for ui

--- python/main.py
+++ python/main.py
@@ -1,34 +1,49 @@
+from typing import Annotated
 from uuid import UUID
 
 import uvicorn
-from fastapi import FastAPI, status, responses
+from fastapi import FastAPI, responses, Request, Form
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 from model import NoteText
 from store import read, save
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.post('/generate')
-def post_generate_phrase(note: NoteText) -> responses.JSONResponse:
+
+@app.get('/generate', response_class=HTMLResponse)
+async def generate(request: Request) -> Jinja2Templates.TemplateResponse:
+    """
+    Запрос на отрисовку страницы
+    """
+    return templates.TemplateResponse(
+        request=request, name='create_note.html'
+    )
+
+
+@app.post('/generate', response_model=NoteText)
+async def post_generate_phrase(request: Request, note_input: Annotated[str, Form()]) -> Jinja2Templates.TemplateResponse:
     """
     Принимает текст и отдает id записки
     """
-    data = {
-        'note_id': save(text=note.text)
-    }
-    return responses.JSONResponse(content=data, status_code=status.HTTP_201_CREATED)
+    note = NoteText(text=note_input)
+    url = f'{request.base_url}secrets/{save(text=note.text)}'
+    return templates.TemplateResponse(
+        request=request, name='return_note_url.html', context={'url': url}
+    )
 
 
 @app.get('/secrets/{note_id}')
-def get_note(note_id: UUID) -> responses.JSONResponse:
+def get_note(request: Request, note_id: UUID) -> Jinja2Templates.TemplateResponse:
     """
     Отдает текст записки и удаляет его
     """
-    data = {
-        'note_text': read(note_id)
-    }
-    return responses.JSONResponse(content=data)
+    return templates.TemplateResponse(
+        request=request, name='note_read.html', context={'text': read(note_id)}
+    )
 
 
 if __name__ == '__main__':
